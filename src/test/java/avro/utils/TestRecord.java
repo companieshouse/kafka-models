a7683fd93af09a487c552ff05c14131cb5fd5b7d
bcullerton <bcullerton@companieshouse.gov.uk>
2023-02-07 16:10:31 +0000

    clean TestRecord to remove attempt

--- src/test/java/avro/utils/TestRecord.java
+++ src/test/java/avro/utils/TestRecord.java
@@ -15,7 +15,6 @@ public class TestRecord extends SpecificRecordBase {
     public static final Schema SCHEMA$ = new Schema.Parser().parse(
         "{\"type\":\"record\",\"name\":\"TestRecord\",\"namespace\":\"testrecord\",\"doc\":\"Test Schema\",\"fields\":[{\"name\":\"data\",\"type\":{\"type\":\"string\",\"avro.java.string\":\"String\"},\"doc\":\"Test Payload\"}]}");
     public String data;
-    public int attempt;
 
     @Override
     public Schema getSchema() {
@@ -24,18 +23,10 @@ public class TestRecord extends SpecificRecordBase {
 
     @Override
     public java.lang.Object get(int field) {
-        switch (field) {
-        case 0: return data;
-        case 1: return attempt;
-        default: throw new org.apache.avro.AvroRuntimeException("Bad index");
-        }
+        return data;
       }
 
-    public void put(int field, java.lang.Object value) {
-        switch (field) {
-        case 0: data = (java.lang.String)value; break;
-        case 1: attempt = (java.lang.Integer)value; break;
-        default: throw new org.apache.avro.AvroRuntimeException("Bad index");
-        }
+    public void put(int field, Object value) {
+        data = value.toString();
     }
 }
